6a30af02a60d941d854c660507d6f71560ea1577
Haruki Yasui <yasui.haruki.74s@gmail.com>
2019-04-25 15:40:25 +0700

    Initial Commit

---
+++ TestFotGKC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFotGKC
+//
+//  Created by 安井春輝 on 4/25/31 H.
+//  Copyright © 31 Heisei haruki yasui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
